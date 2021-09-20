2a4255bebe3553812791eff675af308cc024436d
hujinwen <hujinwen@shizhuang-inc.com>
2021-09-20 20:35:46 +0800

    新建frida-tools子项目

---
+++ frida-tools/src/main/java/com/hujinwen/tool/PrintTool.java
@@ -0,0 +1,52 @@
+package com.hujinwen.tool;
+
+import com.hujinwen.utils.ReflectUtils;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+/**
+ * Created by hu-jinwen on 2021/9/20
+ */
+public class PrintTool {
+
+    /**
+     * 打印出指定类中，所有方法
+     */
+    public static String printMethods(Class<?> clazz) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Class: ");
+        sb.append(clazz.toString());
+        sb.append("\n");
+
+        Method[] methods = ReflectUtils.getDeclaredMethods(clazz);
+        for (Method method : methods) {
+            sb.append(method.toString());
+            sb.append("\n");
+        }
+        sb.append("\n");
+        return sb.toString();
+    }
+
+    /**
+     * 打印出指定对象中所有成员变量，以及变量的值
+     */
+    public static String printFields(Object obj) throws IllegalAccessException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Object: ");
+        sb.append(obj.toString());
+        sb.append("\n");
+
+        Field[] fields = ReflectUtils.getDeclaredFields(obj.getClass());
+        for (Field field : fields) {
+            sb.append(field.toString());
+            sb.append(" => ");
+            sb.append(field.get(obj));
+            sb.append("\n");
+        }
+        sb.append("\n");
+        return sb.toString();
+    }
+
+
+}
